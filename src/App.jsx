22dbfa89da51d70d7c94e9dd1993b5fac833767f
Ayan Kumar <kumarayanatwork@gmail.com>
2026-01-12 14:29:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
